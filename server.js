2031593ab3379fbc1297c84084ae16cedf7d7bda
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-08-08 02:35:37 +0700

    update server.js #each page url

--- server.js
+++ server.js
@@ -30,4 +30,45 @@ app.post('/create-checkout-session', async (req, res) => {
   }
 });
 
+app.post('/create-checkout-session_independent', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: [{
+       price: 'price_1Rka4DRU1fA8NXRMWKwM2fzT',
+       //price: 'price_1RtYZGRc6cDcGXxsuCW33pnW',
+        quantity: 1,
+      }],
+      success_url: 'https://www.correctthecontract.com/independent-contractor?payment=success',
+      cancel_url: 'https://yourdomain.com/cancel',
+    });
+
+    res.json({ url: session.url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Stripe checkout session creation failed' });
+  }
+});
+
+app.post('/create-checkout-session_video', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: [{
+       price: 'price_1Rka4DRU1fA8NXRMWKwM2fzT',
+       //price: 'price_1RtYZGRc6cDcGXxsuCW33pnW',
+        quantity: 1,
+      }],
+      success_url: 'https://www.correctthecontract.com/video-film?payment=success',
+      cancel_url: 'https://yourdomain.com/cancel',
+    });
+
+    res.json({ url: session.url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Stripe checkout session creation failed' });
+  }
+});
 app.listen(4242, () => console.log('Server running on port 4242'));
